fc311728c17d6ff45dd69834875d7c853c002f89
tonymatsumura <antonio.matsumura@aluno.ufop.edu.br>
2025-10-08 10:52:30 -0300

    Add debug logging for API response rows in prices.js

--- api/prices.js
+++ api/prices.js
@@ -3,6 +3,7 @@ export default async function handler(req, res) {
     //const sheetID = "18koeqwdzJ612S8NT701NaM-Z_FwjfibqxWgXH21VuRU";
     const sheetID = "1S_ZnkoVT7W9kFAafS1ApH4AlRsuqVwLi3yAKdpWD-Qg";
     //const sheetName = "Prices";
+    //Spreadsheet  XURL = "https://docs.google.com/spreadsheets/d/1S_ZnkoVT7W9kFAafS1ApH4AlRsuqVwLi3yAKdpWD-Qg"
     const sheetName = "Products";
     const url = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?tqx=out:json&sheet=${sheetName}`;
 
@@ -11,6 +12,7 @@ export default async function handler(req, res) {
 
     const json = JSON.parse(text.substr(47).slice(0, -2));
     const rows = json.table.rows;
+    console.log(rows);
 
     const data = rows.map(row => ({
       produto: row.c[0]?.v || "",
